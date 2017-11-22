9e5c3655f1f64e0156196aff04412fbab5c037d2
Nestor Britez <nestor@britez.me>
2017-11-22 19:15:24 -0300

    Upgrade dependencies

--- src/app/commons/commons.module.ts
+++ src/app/commons/commons.module.ts
@@ -2,16 +2,36 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
+import {
+  MatToolbarModule,
+  MatIconModule,
+  MatButtonModule,
+  MatMenuModule,
+  MatCardModule
+} from '@angular/material';
+
 @NgModule({
   imports: [
     CommonModule,
-    FormsModule
+    FormsModule,
+
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatMenuModule,
+    MatCardModule
   ],
   declarations: [
   ],
   exports: [
     CommonModule,
-    FormsModule
+    FormsModule,
+
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatMenuModule,
+    MatCardModule
   ],
   providers: []
 })
